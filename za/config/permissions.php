2be72454c3c27da27b5d962d31b98e2375f3e5b6
Nyan Lynn Htut <lynnhtut87@gmail.com>
2023-11-21 16:24:48 +0630

    update

--- za/config/permissions.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-    'Dashboard' => [
-        ['en' => 'dashboard-view', 'mm' => 'ထိန်းချုပ်စင်တာ'],
-    ],
-];
--- za/license.md
+++
@@ -1,5 +0,0 @@
-# The license
-
-Copyright (c) Nyan Lynn Htut <lynnhtut87@gmail.com>
-
-...Add your license text here...
