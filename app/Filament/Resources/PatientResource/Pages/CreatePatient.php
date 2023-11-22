b322127fe4ab3cee5242838f2513f361d005ad8a
Nyan Lynn Htut <lynnhtut87@gmail.com>
2023-11-22 10:14:57 +0630

    update for image upload at create

--- app/Filament/Resources/PatientResource/Pages/CreatePatient.php
+++ app/Filament/Resources/PatientResource/Pages/CreatePatient.php
@@ -12,21 +12,28 @@ class CreatePatient extends CreateRecord
 {
     protected static string $resource = PatientResource::class;
 
+    protected $fImage;
+    protected $bImage;
+    protected $sImage = [];
+
     protected function mutateFormDataBeforeCreate(array $data): array
     {
-        $fImage = $data['front_image'];
-        $bImage = $data['back_image'];
-        $sImage = $data['selfie_image'];
-
-        $this->saveImage($fImage, 'front');
-        $this->saveImage($bImage, 'back');
-        foreach ($sImage as $img) {
-            $this->saveImage($img, 'selfie');
-        }
+        $this->fImage = $data['front_image'];
+        $this->bImage = $data['back_image'];
+        $this->sImage = $data['selfie_image'];
 
         unset($data['front_image'], $data['back_image'], $data['selfie_image']);
      
         return $data;
+    }
+
+    protected function afterCreate(): void
+    {
+        $this->saveImage($this->fImage, 'front');
+        $this->saveImage($this->bImage, 'back');
+        foreach ($this->sImage as $img) {
+            $this->saveImage($img, 'selfie');
+        }
     }
 
     protected function saveImage($path, $type)
@@ -35,7 +42,7 @@ class CreatePatient extends CreateRecord
             'url' => $path,
             'type' => $type,
             'imageable_type' => $this->getModel(), 
-            'imageable_id' => $this->record->id/// Need to fix,,bcoz record is not created
+            'imageable_id' => $this->record->id
         ]);
     }
 }
